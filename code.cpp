4082d250c59b49df45f5daf8255014bddb990b13
sameekshamalav <96688960+sameekshamalav@users.noreply.github.com>
2023-12-04 19:43:51 +0530

    Initialized

---
+++ code.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// A technical/cultural fest management platform for a college using Object Oriented Programming
+
+class Event;
+class Workforce;
+class Participant;
+class Coordinator;
+class CoreMember;
+
+int main(){
+
+}
